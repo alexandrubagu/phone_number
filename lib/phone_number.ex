f791c2865c46927ca56d3786483fc630ba276ff3
Alexandru Bagu <alexandrubagu87@gmail.com>
2024-10-29 14:40:51 +0200

    first commit

---
+++ lib/phone_number.ex
@@ -0,0 +1,18 @@
+defmodule PhoneNumber do
+  @moduledoc """
+  Documentation for `PhoneNumber`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoneNumber.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
